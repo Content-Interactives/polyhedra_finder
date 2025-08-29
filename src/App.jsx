deacae21ff632702a992cb50cb7a08cf74ff2db2
User <user@example.com>
2025-08-29 10:04:29 -0700

    added app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PolyhedraFinder from './components/PolyhedraFinder'
+
+
+function App() {
+  return (
+      <div className="App">
+        <PolyhedraFinder />
+      </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/PolyhedraFinder.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+
+const PolyhedraFinder = () => {
+
+
+	return (
+        <div>test</div>
+)
+};
+
+
+export default PolyhedraFinder;
